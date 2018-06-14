ff6623bed4e7d4a67d0a750717aeeb000ea2aac7
Me <me@example.com>
2018-06-14 14:59:47 +0000

    Commit made from Membrane web

---
+++ index.js
@@ -0,0 +1,9 @@
+const { root } = program.refs
+
+export async function init() {
+  // Called when the program is run
+}
+
+export async function timer({ key }) {
+  // Called every time a timer fires
+}
