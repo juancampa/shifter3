6a91997c1570aa1c75a04d2c5c13dafe8c8d2bcb
Me <me@example.com>
2018-06-14 15:04:08 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const { root } = program.refs
 
 export async function init() {
   // Called when the program is run
+  return twilio.messages.sendSms({
+    from: '+17864605016',
+    to: '+17863005554',
+    body: 'Shifter - init'
+  })
 }
 
 export async function timer({ key }) {
