69b1ff6ccc649671baf45eaf1f8daabc8833c241
Me <me@example.com>
2018-07-01 03:38:59 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const { root, twilio, shiftTable } = program.refs
+const { root, twilio, employeeTable, talk } = program.refs
 
 export async function init() {
-  // Called when the program is run
-  await twilio.sendSms({
-    to: '+17863005554',
-    body: 'Shifter - initialized'
-  })
-
   await twilio.smsReceived.subscribe('onSms');
 }
 
+// Compare phone numbers for equality
 function phoneEqual(a, b) {
   const re = /(^\+1|[^0-9])/g;
   return a && b && a.replace(re, '') === b.replace(re, '');
@@ -24,45 +19,15 @@ export async function onSms({ sender, args }) {
 }
 
 export const Root = {
-  async shifts({ args }) {
-    const records = await shiftTable.records.page.items.query(`{ fields }`);
-    return records.map((record) => {
-      const fields = JSON.parse(record.fields);
-      const key = Object.keys(fields).find((k) => k.toLowerCase() == args.day.toLowerCase());
-      const [start, end] = fields[key].split('-');
-      return {
-        start,
-        end,
-        name: fields.Name,
-        phone: fields.Phone,
-      };
-    });
-  },
-
   async employees({ args }) {
-    const records = await shiftTable.records.page.items.query(`{ fields }`);
-    return records.map((record) => {
-      const fields = JSON.parse(record.fields);
-      return {
-        name: fields.Name,
-        phone: fields.Phone,
-      };
-    });
-  }
-}
-
-export const ShiftCollection = {
-  one({ args, source }) {
-    return source.find((shift) => shift.name === args.name);
-  },
-  items({ source }) {
-    return source;
-  }
-}
-
-export const Shift = {
-  self({ self, source, parent }) {
-    return self || parent.ref.pop().push('one', { name: source.name });
+    return employeeTable.records.perItem(`{ fields }`)
+      map((record) => {
+        const fields = JSON.parse(record.fields);
+        return {
+          name: fields.Name,
+          phone: fields.Phone,
+        };
+      });
   }
 }
 
@@ -77,13 +42,9 @@ export const EmployeeCollection = {
 
 export const Employee = {
   self({ self, source, parent }) {
-    return self || parent.ref.pop().push('one', { name: source.name });
+    return parent.ref.pop().push('one', { name: source.name });
   },
-
   async sendMessage({ self, args }) {
-    if (!args.text) {
-      return;
-    }
     return twilio.sendSms({
       to: await self.phone.query(),
       body: args.text,
