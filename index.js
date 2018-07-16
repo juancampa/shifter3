6b3bacdc1a21e798df233f39973cb04c35dcdf63
Me <me@example.com>
2018-07-16 01:01:53 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export const Employee = {
 export async function onReply({ args, sender, unsubscribe }) {
   const { answer, context } = args;
   const { name } = context.args;
-  const { answer: { time } } = sender.query(`{ answer { time } }`)
+  const { answer: { time } } = await sender.query(`{ answer { time } }`)
 
   // Register the reply
   await root.employees.one({ name }).registerAnswer({ answer, time })
